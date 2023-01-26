371a90d096e90a659e52ba4dcb2e608a2105d1ec
hatcoceo <39942640+hatcoceo@users.noreply.github.com>
2023-01-26 08:06:13 +0700

    Create dang_ky.php

---
+++ dang_ky.php
@@ -0,0 +1,4 @@
+<label>Username</label><input type="text" name="username" >
+<label>Password</label><input type="text" name="pass1" >
+<label>Nhap lai password</label><input type="text" name="pass2" >
+<button type="submit">Dang Ky</button>
